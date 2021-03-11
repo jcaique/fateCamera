37381e7d70bb3fd8e7054c575585479fa49bfa6a
jcaique <jose.silva448@fatec.sp.gov.br>
2021-03-11 00:36:51 -0300

    Adding the button to hide the modal, and hard sofering with position

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useRef } from 'react';
-import { Image, Modal, Platform, SafeAreaView, TouchableOpacity, View, StyleSheet } from 'react-native'
+import { Text, Image, Modal, Platform, SafeAreaView, TouchableOpacity, View, StyleSheet } from 'react-native'
 import { Camera } from 'expo-camera'
 import { Ionicons } from '@expo/vector-icons'
 import * as Permissions from 'expo-permissions'
@@ -10,16 +10,22 @@ import Cabecalho from './componentesCriados/Cabecalho' //por default ele pega o 
 export default function App() {
   //status de acesso à camera
   const [temPermissao, setTemPermissao] = useState(null)
+
   //referencia da camera
   const cameraReferencia = useRef(null)
+
   //icones padroes que serao exibidos, md material design
   const [iconePadrao, setIconePadrao] = useState('md')
+
   //tipo da camera (frontal ou trasiera)
   const [tipoCamera, setTipoCamera] = useState(Camera.Constants.Type.back)
+
   //status inicial do flash
   const [tipoFlash, setTipoFlash] = useState(Camera.Constants.FlashMode.off)
+
   //foto capturada
   const [fotoCapturada, setFotoCapturada] = useState(null)
+
   //exibe ou nao modal - controle do modal
   const [exibeModal, setExibeModal] = useState(false)
 
@@ -27,10 +33,10 @@ export default function App() {
   useEffect(() => {
     switch (Platform.OS) {
       case 'android':
-        setIconePadrao('md')
+        setIconePadrao('md') //android
         break
       case 'ios':
-        setIconePadrao('ios')
+        setIconePadrao('ios') //ios
         break
     }
   })
@@ -47,7 +53,7 @@ export default function App() {
           setTemPermissao(status === 'granted')
         }
       }
-    )() //
+    )() //promisse
   }, []) /** quando o array "segundo argumento" está vazio, o useEffect é executado apenas uma vez */
 
   if (temPermissao === false) {
@@ -56,12 +62,14 @@ export default function App() {
 
   async function tirarFoto() {
     if (cameraReferencia) {
+      Camera
       const options = {
-        quality: 0.5,
-        skipProcessing: true
+        quality: 1,
+        skipProcessing: false,
+        ratio: '16:9',
       }
       const foto = await cameraReferencia.current.takePictureAsync(options)
-      setFotoCapturada(foto.uri)
+      setFotoCapturada(foto.uri) //takePictureAsync retorna o uri da foto, aqui 
       setExibeModal(true)
     }
   }
@@ -77,31 +85,51 @@ export default function App() {
         flashMode={tipoFlash}
         ref={cameraReferencia}
       >
+
         <View style={estilos.camera}>
           {/** botao camera */}
           <TouchableOpacity style={estilos.touch} onPress={tirarFoto}>
             <Ionicons name={`${iconePadrao}-camera`} size={40} color="#9e9e9e" />
           </TouchableOpacity>
 
+          {/* Botao camera frontal/traseira */}
           <TouchableOpacity style={estilos.touch} onPress={() => {
             setTipoCamera(
-              tipoCamera === Camera.Constants.Type.back ? Camera.Constants.Type.front : Camera.Constants.Type.back
-            )
+              tipoCamera === Camera.Constants.Type.back ? Camera.Constants.Type.front : Camera.Constants.Type.back)
           }}>
             <Ionicons name={`${iconePadrao}-camera-reverse`} size={40} color='#e9e9e9' />
+          </TouchableOpacity>
+
+          {/*Botao flash */}
+          <TouchableOpacity style={estilos.touch} onPress={() => {
+            setTipoFlash(
+              tipoFlash === Camera.Constants.FlashMode.on ? Camera.Constants.FlashMode.off : Camera.Constants.FlashMode.on
+            )
+          }}
+          >
+            <Ionicons name={tipoFlash === Camera.Constants.FlashMode.on ? `${iconePadrao}-flash` : `${iconePadrao}-flash-off`}
+              size={40} color='#d9e9'
+            />
           </TouchableOpacity>
         </View>
       </Camera>
 
-      {/**monstrando a foto */}
+      {/*monstrando a foto */}
       <Modal animationType='slide' transparent={true} visible={exibeModal}>
         <View style={estilos.modalView}>
-          <Image source={{ uri: fotoCapturada }}
-            style={{ width: '90%', height: '50%', borderRadius: 20 }}
-          />
+
+          <Image source={{ uri: fotoCapturada }} style={{ width: '90%', height: '50%' }} />
+
+          {/*botao para fechar o modal via documentação*/}
+          <TouchableOpacity style={[estilos.botaoFecharModal]} onPress={() => { setExibeModal(false) }}>
+            <Text>Fechar</Text>
+            <Ionicons name={`${iconePadrao}-close`} size={20} color='rgb(999,0,0)' />
+          </TouchableOpacity>
+
         </View>
       </Modal>
-    </SafeAreaView>
+
+    </SafeAreaView >
   );
 }
 
@@ -121,13 +149,29 @@ const estilos = StyleSheet.create({
 
   touch: {
     margin: 20,
-
   },
+
   modalView: {
     margin: 10,
     backgroundColor: 'white',
     padding: 15,
-    opacity: 0.9,
-    alignItems: 'center'
+    opacity: 1,
+    alignItems: 'center',
+    borderTopStartRadius: 20,
+    borderTopEndRadius: 20
+  },
+
+  botaoFecharModal: {
+    alignSelf: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+    width: 80,
+    height: 30,
+    borderRadius: 10,
+    borderWidth: 1,
+    fontSize: 20,
+    bottom: '-50%',
+    backgroundColor: '#e2a1d5d2'
   }
 })
